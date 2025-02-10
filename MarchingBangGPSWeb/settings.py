b519c0df3eb89357b26b92b917f34248e771da55
Vladislav Lebedev <vladislav_lebedev@tamu.edu>
2025-02-10 01:41:50 -0600

    Hosting site changes

--- MarchingBangGPSWeb/settings.py
+++ MarchingBangGPSWeb/settings.py
@@ -82,8 +82,14 @@ WSGI_APPLICATION = 'MarchingBangGPSWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('Database_Name'),
+        'USER': os.environ.get('Database_User'),
+        'PASSWORD': os.environ.get('Database_Password'),
+        'HOST': os.environ.get('Database_Host'),
+        'PORT': os.environ.get('Database_Port')
     }
 }
 
