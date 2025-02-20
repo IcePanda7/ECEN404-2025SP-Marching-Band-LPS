883eaaf7398f297c36f1953230e35048a5227ab1
Vladislav Lebedev <vladislav_lebedev@tamu.edu>
2025-02-20 11:47:30 -0600

    Database changes

--- assets/Person.js
+++ assets/Person.js
@@ -166,9 +166,3 @@ function ShowMembers(){                                                     // f
     }
 }
 
-/*
-window.onbeforeunload = function (){                                    // Before loading the next html
-    fetch('/BandField/stop_watchdog/')                                  // Retrieve the stop_watchdog function
-        .then(response => response.json())                          // Stop watchdog from running
-}
-*/
