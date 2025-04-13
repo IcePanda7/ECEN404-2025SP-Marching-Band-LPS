6975598529938eac59a928b85e249ba8c55851a7
Vladislav Lebedev <vladislav_lebedev@tamu.edu>
2025-04-13 18:59:46 -0500

    Finishing touches

--- assets/Person.js
+++ assets/Person.js
@@ -164,7 +164,7 @@ function RandomMovement(marcher, dot){                                          
     console.log("Field Width:", field_width);
 
     x_direction = ((marcher.x_coordinate * (field_width / 1203)) / 9.2) + horizontal_offset;                      // Equation for the marcher's x direction for different field sizes
-    y_direction = menu_height + title_height + field_height - ((marcher.y_coordinate * (field_height/622.61)) / 7.33);            // Equation for the marcher's y direction for different field sizes
+    y_direction = menu_height + title_height + field_height - ((marcher.y_coordinate * (field_height/622.61)) / 7.1);            // Equation for the marcher's y direction for different field sizes
 
 
     // Case 1: x out of bounds left
