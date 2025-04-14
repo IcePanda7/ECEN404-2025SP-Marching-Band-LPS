bcc5e2a5a8e749f63b1eec9296be20615b7a388f
Vladislav Lebedev <vladislav_lebedev@tamu.edu>
2025-04-13 19:09:26 -0500

    Finishing touches

--- assets/Person.js
+++ assets/Person.js
@@ -58,7 +58,7 @@ function StartBlinking(){                                                 // Def
                     tooltip.classList.add('show');                                             // Show the description box
                     const rect = dot.getBoundingClientRect();                        // To position the box
                     tooltip.style.left = `${rect.left-190}px`;                                 // Position from the left
-                    tooltip.style.top = `${rect.top-10}px`;                                   // Position from the top
+                    tooltip.style.top = `${rect.top-290}px`;                                   // Position from the top
                 });
                 dot.addEventListener('mouseleave', function(){              // For when a user hover off a dot
                     tooltip.classList.remove('show');                                   // Don't show the description box
